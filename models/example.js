46d4231b4968bcf77da9f7a89b8c2661bc05b8e9
Nolan Hewitt <nolanhewitt@aol.com>
2019-09-27 12:26:15 -0500

    add

--- models/example.js
+++ models/example.js
@@ -1,7 +1,10 @@
 module.exports = function(sequelize, DataTypes) {
   var Example = sequelize.define("Example", {
     text: DataTypes.STRING,
-    description: DataTypes.TEXT
+    description: DataTypes.TEXT,
+    creator: DataTypes.TEXT,
+    category: DataTypes.TEXT,
+    assignee: DataTypes.TEXT
   });
   return Example;
 };
