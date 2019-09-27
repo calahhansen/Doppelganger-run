2a35d8b9a2e00d83f209a7f02b25933b5eeee2e5
Jack Meuwissen <jmeuwissen42@gmail.com>
2019-09-27 13:09:33 -0500

    refactored from 'example' naming to 'task' naming

--- models/task.js
+++ models/task.js
@@ -1,10 +1,10 @@
 module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
+  var Task = sequelize.define("Task", {
     text: DataTypes.STRING,
     description: DataTypes.TEXT,
     creator: DataTypes.TEXT,
     category: DataTypes.TEXT,
     assignee: DataTypes.TEXT
   });
-  return Example;
+  return Task;
 };
