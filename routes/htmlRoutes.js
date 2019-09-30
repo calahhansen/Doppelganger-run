deaf767a7dbce6eddbba4c7ddacf3f1219e038ec
Nolan Hewitt <nolanhewitt@aol.com>
2019-09-30 13:38:20 -0500

    fixed a lot of conflict bugs

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -25,38 +25,48 @@ module.exports = function(app) {
   });
 
   // Render 404 page for any unmatched routes
+  app.get("/profile", function(req, res) {
+    res.render("profile");
+  });
+
+  // Render 404 page for any unmatched routes
+  app.get("/logIn", function(req, res) {
+    res.render("logIn");
+  });
+
+  // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
   });
 };
 
-//===============================Login/Register ROUTES========================== (CALAH set-up PLEASE PLEASE double check....I was guessing a lot!)
-// Requiring path to so we can use relative routes to our HTML files
-var path = require("path");
+// //===============================Login/Register ROUTES========================== (CALAH set-up PLEASE PLEASE double check....I was guessing a lot!)
+// // Requiring path to so we can use relative routes to our HTML files
+// var path = require("path");
 
-// Requiring our custom middleware for checking if a user is logged in
-var isAuthenticated = require("../config/middleware/isAuthenticated");
+// // Requiring our custom middleware for checking if a user is logged in
+// var isAuthenticated = require("../config/middleware/isAuthenticated");
 
-module.exports = function(app) {
-  app.get("/", function(req, res) {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/index");
-    }
-    res.sendFile(path.join(__dirname, "../public/html/logIn.html"));
-  });
+// module.exports = function(app) {
+//   app.get("/", function(req, res) {
+//     // If the user already has an account send them to the members page
+//     if (req.user) {
+//       res.redirect("/index");
+//     }
+//     res.sendFile(path.join(__dirname, "../public/html/logIn.html"));
+//   });
 
-  app.get("/login", function(req, res) {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/index");
-    }
-    res.sendFile(path.join(__dirname, "../public/html/login.html"));
-  });
+//   app.get("/login", function(req, res) {
+//     // If the user already has an account send them to the members page
+//     if (req.user) {
+//       res.redirect("/index");
+//     }
+//     res.sendFile(path.join(__dirname, "../public/html/login.html"));
+//   });
 
-  // Here we've add our isAuthenticated middleware to this route.
-  // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/index", isAuthenticated, function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/html/logIn.html"));
-  });
-};
+//   // Here we've add our isAuthenticated middleware to this route.
+//   // If a user who is not logged in tries to access this route they will be redirected to the signup page
+//   app.get("/index", isAuthenticated, function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/html/logIn.html"));
+//   });
+// };
