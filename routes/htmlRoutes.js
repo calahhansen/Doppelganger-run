885be4123fceb085f3f1a0abd0f2c202fc9a6bff
Calah Hansen <calah.hansen@gmail.com>
2019-10-02 21:46:54 -0500

    updated index.handlebars removed all except body

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -89,34 +89,3 @@ module.exports = function(app) {
     res.render("404");
   });
 };
-
-// //===============================Login/Register ROUTES========================== (CALAH set-up PLEASE PLEASE double check....I was guessing a lot!)
-// // Requiring path to so we can use relative routes to our HTML files
-// var path = require("path");
-
-// // Requiring our custom middleware for checking if a user is logged in
-// var isAuthenticated = require("../config/middleware/isAuthenticated");
-
-// module.exports = function(app) {
-//   app.get("/", function(req, res) {
-//     // If the user already has an account send them to the members page
-//     if (req.user) {
-//       res.redirect("/index");
-//     }
-//     res.sendFile(path.join(__dirname, "../public/html/logIn.html"));
-//   });
-
-//   app.get("/login", function(req, res) {
-//     // If the user already has an account send them to the members page
-//     if (req.user) {
-//       res.redirect("/index");
-//     }
-//     res.sendFile(path.join(__dirname, "../public/html/login.html"));
-//   });
-
-//   // Here we've add our isAuthenticated middleware to this route.
-//   // If a user who is not logged in tries to access this route they will be redirected to the signup page
-//   app.get("/index", isAuthenticated, function(req, res) {
-//     res.sendFile(path.join(__dirname, "../public/html/logIn.html"));
-//   });
-// };
